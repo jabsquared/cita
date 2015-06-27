dcaaed734350f7b2307088cdf09808e55bcc1853
Jim Tryon <tryonjf@gmail.com>
2015-06-27 15:47:55 -0700

    Added auto populated list

---
+++ appointly/www/js/app.js
@@ -0,0 +1,26 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var myApp = angular.module('starter', ['ionic']);
+
+myApp.controller('MainCtrl', function() {
+	this.items = [];
+	for (var i = 1; i <= 20; i++) {
+		this.items.push(i);
+	}
+});
+
+myApp.run(function($ionicPlatform) {
+	$ionicPlatform.ready(function() {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	});
+})
