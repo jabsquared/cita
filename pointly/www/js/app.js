e81d08c55f29d8f457be55271c50fffb5bd8167c
Louis Vichy <lgvichy@gmail.com>
2015-06-27 18:44:13 -0700

    Check this out: http://devdactic.com/simple-login-example-with-ionic-and-angularjs/

--- pointly/www/js/app.js
+++ pointly/www/js/app.js
@@ -1,32 +1,86 @@
-// Ionic Starter App
+angular.module('ionicApp', ['ionic'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic', 'ui.router', 'starter.controllers'])
+.config(function($stateProvider, $urlRouterProvider) {
 
-//Configure routes using 2 dependencies
-app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('signin', {
+      url: '/sign-in',
+      templateUrl: 'templates/sign-in.html',
+      controller: 'SignInCtrl'
+    })
+    .state('forgotpassword', {
+      url: '/forgot-password',
+      templateUrl: 'templates/forgot-password.html'
+    })
+    .state('tabs', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    })
+    .state('tabs.home', {
+      url: '/home',
+      views: {
+        'home-tab': {
+          templateUrl: 'templates/home.html',
+          controller: 'HomeTabCtrl'
+        }
+      }
+    })
+    .state('tabs.facts', {
+      url: '/facts',
+      views: {
+        'home-tab': {
+          templateUrl: 'templates/facts.html'
+        }
+      }
+    })
+    .state('tabs.facts2', {
+      url: '/facts2',
+      views: {
+        'home-tab': {
+          templateUrl: 'templates/facts2.html'
+        }
+      }
+    })
+    .state('tabs.about', {
+      url: '/about',
+      views: {
+        'about-tab': {
+          templateUrl: 'templates/about.html'
+        }
+      }
+    })
+    .state('tabs.navstack', {
+      url: '/navstack',
+      views: {
+        'about-tab': {
+          templateUrl: 'templates/nav-stack.html'
+        }
+      }
+    })
+    .state('tabs.contact', {
+      url: '/contact',
+      views: {
+        'contact-tab': {
+          templateUrl: 'templates/contact.html'
+        }
+      }
+    });
 
 
-  $stateProvider.state('home', {
-    url: '/home',
-    template: '<p>Hello, world!</p>'
-  })
+   $urlRouterProvider.otherwise('/sign-in');
 
-  $urlRouterProvider.otherwise('/')
 })
 
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+.controller('SignInCtrl', function($scope, $state) {
+
+  $scope.signIn = function(user) {
+    console.log('Sign-In', user);
+    $state.go('tabs.home');
+  };
 
 })
+
+.controller('HomeTabCtrl', function($scope) {
+  console.log('HomeTabCtrl');
+});
--- pointly/www/js/controllers.js
+++
@@ -1,5 +0,0 @@
-angular.module('starter.controllers', [])
-
-.controller('SchedualeCtrl', function($scope) {
-  $scope.test = 'FU!';
-});
