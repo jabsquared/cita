64a8fe85a0d612cba19a14ce03094aba94e17233
Jim Tryon <tryonjf@gmail.com>
2015-06-28 02:50:29 -0700

    Added schedule times and AM text

--- appointly/www/js/app.js
+++ appointly/www/js/app.js
@@ -1,31 +1,42 @@
-// Ionic Starter App
+angular.module('ionicApp', ['ionic'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-var myApp = angular.module('starter', ['ionic']);
-
-myApp.controller('MainCtrl', function($scope) {
-	this.items = [];
-	for (var i = 0; i <= 20; i++) {
-		this.items.push(i);
-	}
-    $scope.listCanSwipe = true
-});
-
-myApp.config(function($ionicConfigProvider) {
-  $ionicConfigProvider.tabs.position('top');
-});
-
-myApp.run(function($ionicPlatform) {
-	$ionicPlatform.ready(function() {
-		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-		// for form inputs)
-		if (window.cordova && window.cordova.plugins.Keyboard) {
-			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-		}
-		if (window.StatusBar) {
-			StatusBar.styleDefault();
-		}
-	});
+.config(function($ionicConfigProvider) {
+	$ionicConfigProvider.tabs.position('top');
 })
+
+.controller('MyCtrl', function($scope) {
+
+  $scope.data = {
+    showDelete: false
+  };
+
+  $scope.edit = function(item) {
+    alert('Edit Item: ' + item.id);
+  };
+  $scope.share = function(item) {
+    alert('Share Item: ' + item.id);
+  };
+
+  $scope.moveItem = function(item, fromIndex, toIndex) {
+    $scope.items.splice(fromIndex, 1);
+    $scope.items.splice(toIndex, 0, item);
+  };
+
+  $scope.onItemDelete = function(item) {
+    $scope.items.splice($scope.items.indexOf(item), 1);
+  };
+
+  $scope.items = [
+    { id: 11 },
+    { id: 12 },
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+  ];
+
+	/* iterate through array and append text "AM" to values */
+	
+
+});
