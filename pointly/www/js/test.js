e3c71dc1cd44b056168e51107b21e1ce699ae9ff
Louis Vichy <lgvichy@gmail.com>
2015-07-01 21:35:06 -0700

    Sign up Page done with minimal functionality, now you can sign in with phone and email

--- pointly/www/js/test.js
+++ pointly/www/js/test.js
@@ -18,78 +18,136 @@ angular.module('ionicApp', ['ionic'])
       templateUrl: 'templates/app-form.html',
       controller: 'FormCtrl'
     })
-    .state('signup',{
+    .state('signup', {
       url: '/sign-up',
-      templateUrl : 'templates/sign-up.html',
-      controller : 'SignUpCtrl'
+      templateUrl: 'templates/sign-up.html',
+      controller: 'SignUpCtrl'
     });
   $urlRouterProvider.otherwise('/sign-in');
 })
 
-.controller('ScheduleCtrl', function($scope, $http){
+.controller('ScheduleCtrl', function($scope, $http) {
   $scope.stories = [];
   $scope.appointments = info;
   $scope.doRefresh = function() {
     $http.get('/schedule')
-     .success(function(/*newItems*/) {
-      //  $scope.items = newItems;
-      $scope.appointments = info;
-     })
-     .finally(function() {
-       // Stop the ion-refresher from spinning
-       $scope.$broadcast('scroll.refreshComplete');
-     });
-    };
+      .success(function( /*newItems*/ ) {
+        //  $scope.items = newItems;
+        $scope.appointments = info;
+      })
+      .finally(function() {
+        // Stop the ion-refresher from spinning
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+  };
 })
 
-.controller('SignUpCtrl', function($http, $scope, $state, $ionicPopup){
+.controller('SignUpCtrl', function($http, $scope, $state, $ionicPopup) {
   $scope.showAlert = function(title, body) {
-   var alertPopup = $ionicPopup.alert({
-     title: title,
-     template: body
-   });
-   alertPopup.then(function(res) {
-     console.log('Error');
-   });
- };
+    var alertPopup = $ionicPopup.alert({
+      title: title,
+      template: body
+    });
+    alertPopup.then(function(res) {
+      $state.go('signin');
+    });
+  };
+
+  $scope.signup = function(name, email, phone, pass) {
+
+    var send = {
+        // "name"      : name,
+        // "email"     : email,
+        // "phone"     : phone,
+        // "pass"      : pass,
+        "id"     : phone,
+        "password"      : pass,
+    }
+
+    console.log(JSON.stringify(send));
+
+    $.ajax({
+      url: 'https://api-us.clusterpoint.com/100600/User_Accounts/_insert',
+      type: 'POST',
+      dataType: 'json',
+      data: JSON.stringify(send),
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('Authorization', 'Basic ' + btoa('bpshonyak@live.com:Password01'));
+      },
+      success: function(data) {
+        if (typeof success != 'undefined') {
+          success(data);
+        }
+        console.log(data);
+        $scope.showAlert('Done!','You are in!');
+      },
+      fail: function(data) {
+        alert('No!');
+        alert(data.error);
+        console.log('Fail!');
+        if (typeof fail != 'undefined') {
+          fail(data);
+        }
+      }
+    })
+
+    // users.done(function (data){
+    //   var d = data.documents;
+    //   //----------------------------------------------------------
+    //   console.log(d);
+    //   var result = $.grep(d, function(e) {
+    //     return e.id == number && e.password == password;
+    //   });
+    //   console.log(result);
+    //   if (result.length == 0) {
+    //     $scope.showAlert('Error', 'Incorrect Number or Password!');
+    //   } else if (result.length == 1) {
+    //     $state.go('sign-in');
+    //   } else {
+    //     $scope.showAlert('Error', 'Duplicate Users Found!');
+    //   }
+    // })
+  }
+
+
 })
 
 .controller('SignInCtrl', function($http, $scope, $state, $ionicPopup) {
 
   $scope.showAlert = function(title, body) {
-   var alertPopup = $ionicPopup.alert({
-     title: title,
-     template: body
-   });
-   alertPopup.then(function(res) {
-     console.log('Error');
-   });
- };
+    var alertPopup = $ionicPopup.alert({
+      title: title,
+      template: body
+    });
+    alertPopup.then(function(res) {
+      console.log('Error');
+    });
+  };
 
   console.log('Enter SingInCtrl');
   $scope.url = '#';
   $scope.acc_number = '';
   $scope.acc_password = '';
-  $scope.login = function (number, password){
+  $scope.login = function(number, password) {
     console.log('Enter login function');
     console.log('number: ' + number);
     console.log('password: ' + password);
     var users = $.ajax({
-      url       : 'https://api-us.clusterpoint.com/100600/User_Accounts/_list_last',
-      type      : 'GET',
-      dataType  : 'json',
+      url: 'https://api-us.clusterpoint.com/100600/User_Accounts/_list_last',
+      type: 'GET',
+      dataType: 'json',
       // data      : '{"query": "<name>Test</name>"}',
-      beforeSend: function (xhr) {
+      beforeSend: function(xhr) {
         xhr.setRequestHeader('Authorization', 'Basic ' + btoa('bpshonyak@live.com:Password01'));
       },
-      success   : function (data) {
+      success: function(data) {
         console.log('Retrived Users!');
         if (typeof success != 'undefined') {
           // jQuery.parseJSON(doc.responseJSON.documents.toSource();
           success(data);
         }
       },
-      fail      : function (data) {
+      fail: function(data) {
         alert(data.error);
         console.log('Fail!');
         if (typeof fail != 'undefined') {
@@ -98,11 +156,13 @@ angular.module('ionicApp', ['ionic'])
       }
     });
 
-    users.done (function (data) {
+    users.done(function(data) {
       var d = data.documents;
       //----------------------------------------------------------
       console.log(d);
-      var result = $.grep(d, function(e){ return e.id == number && e.password == password; });
+      var result = $.grep(d, function(e) {
+        return e.id == number && e.password == password;
+      });
       console.log(result);
       if (result.length == 0) {
         $scope.showAlert('Error', 'Incorrect Number or Password!');
@@ -130,131 +190,131 @@ angular.module('ionicApp', ['ionic'])
 
     $scope.app_id;
     $scope.app_name;
-    $scope.app_img_p='multicare.png';
+    $scope.app_img_p = 'multicare.png';
     $scope.app_date = new Date();
     $scope.app_time = $scope.app_date;
 
-  $scope.go = function(app_id, app_date, app_name, app_time, app_location) {
-    app_date = app_date.toString();
-    app_time = app_time.toString();
+    $scope.go = function(app_id, app_date, app_name, app_time, app_location) {
+      app_date = app_date.toString();
+      app_time = app_time.toString();
 
-    // console.log('Passed into function:');
-    // console.log(app_id);
-    // console.log(app_date);
-    // console.log(app_name);
-    // console.log(app_time);
-    // var fullTime = app_time.split(" ");
-    // app_time = fullTime[0];
-    // var app_zone = fullTime[1];
-    // app_date(new Date(), "mmmm dS, yyyy");
+      // console.log('Passed into function:');
+      // console.log(app_id);
+      // console.log(app_date);
+      // console.log(app_name);
+      // console.log(app_time);
+      // var fullTime = app_time.split(" ");
+      // app_time = fullTime[0];
+      // var app_zone = fullTime[1];
+      // app_date(new Date(), "mmmm dS, yyyy");
 
-    // console.log('Fulltime: ');
-    // console.log(fullTime);
-    // console.log('Time: ');
-    // console.log(app_time);
-    // console.log('Zone: ');
-    // console.log(app_zone);
+      // console.log('Fulltime: ');
+      // console.log(fullTime);
+      // console.log('Time: ');
+      // console.log(app_time);
+      // console.log('Zone: ');
+      // console.log(app_zone);
 
-  var send = {
-    "id"        : app_id,
-    "name"      : app_name,
-    "img_p"     : $scope.app_img_p,
-    "date"      : app_date,
-    "time"      : app_time,
-    "location"  : app_location
-  }
-
-  console.log(send);
-
-  $scope.showAlert = function() {
-   var alertPopup = $ionicPopup.alert({
-     title: 'All done!',
-     template: 'Appointment Requested'
-   });
-   alertPopup.then(function(res) {
-     $state.go('schedule');
-     console.log('Thank you for not eating my delicious ice cream cone');
-    });
-  };
-
-  $.ajax({
-    //removed the /2 from url.
-    url: 'https://api-us.clusterpoint.com//100600/Appointly/_insert',
-    type: 'POST',
-    dataType: 'json',
-    data: JSON.stringify(send),
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader('Authorization', 'Basic ' + btoa('bpshonyak@live.com:Password01'));
-    },
-    success: function(data) {
-      if (typeof success != 'undefined') {
-        success(data);
+      var send = {
+        "id": app_id,
+        "name": app_name,
+        "img_p": $scope.app_img_p,
+        "date": app_date,
+        "time": app_time,
+        "location": app_location
       }
-      $scope.showAlert();
-    },
-      fail: function(data) {
-        alert('No!');
-        alert(data.error);
-        console.log('Fail!');
-        if (typeof fail != 'undefined') {
-          fail(data);
+
+      console.log(send);
+
+      $scope.showAlert = function() {
+        var alertPopup = $ionicPopup.alert({
+          title: 'All done!',
+          template: 'Appointment Requested'
+        });
+        alertPopup.then(function(res) {
+          $state.go('schedule');
+          console.log('Thank you for not eating my delicious ice cream cone');
+        });
+      };
+
+      $.ajax({
+        //removed the /2 from url.
+        url: 'https://api-us.clusterpoint.com//100600/Appointly/_insert',
+        type: 'POST',
+        dataType: 'json',
+        data: JSON.stringify(send),
+        beforeSend: function(xhr) {
+          xhr.setRequestHeader('Authorization', 'Basic ' + btoa('bpshonyak@live.com:Password01'));
+        },
+        success: function(data) {
+          if (typeof success != 'undefined') {
+            success(data);
+          }
+          $scope.showAlert();
+        },
+        fail: function(data) {
+          alert('No!');
+          alert(data.error);
+          console.log('Fail!');
+          if (typeof fail != 'undefined') {
+            fail(data);
+          }
         }
-      }
-    })
+      })
 
 
-    $.ajax({
-      url: 'http://appointlysmsserver.mybluemix.net/data',
-      type: 'POST',
-      contentType: "text/plain; charset=utf-8",
-      dataType: 'text',
-      data: send.name + " scheduled an appoinment with you at " + send.time + " on " + send.date + ", location: " + send.location,
-      // beforeSend: function(xhr) {
-      //   xhr.setRequestHeader('Authorization', 'Basic ' + btoa('bpshonyak@live.com:Password01'));
-      // },
-      success: function(data) {
-        alert("Submitted!");
-        if (typeof success != 'undefined') {
-          // jQuery.parseJSON(doc.responseJSON.documents.toSource());
-          success(data);
+      $.ajax({
+        url: 'http://appointlysmsserver.mybluemix.net/data',
+        type: 'POST',
+        contentType: "text/plain; charset=utf-8",
+        dataType: 'text',
+        data: send.name + " scheduled an appoinment with you at " + send.time + " on " + send.date + ", location: " + send.location,
+        // beforeSend: function(xhr) {
+        //   xhr.setRequestHeader('Authorization', 'Basic ' + btoa('bpshonyak@live.com:Password01'));
+        // },
+        success: function(data) {
+          alert("Submitted!");
+          if (typeof success != 'undefined') {
+            // jQuery.parseJSON(doc.responseJSON.documents.toSource());
+            success(data);
+          }
+        },
+        fail: function(data) {
+          alert('No!');
+          alert(data.error);
+          console.log('Fail!');
+          if (typeof fail != 'undefined') {
+            fail(data);
+          }
         }
-      },
-      fail: function(data) {
-        alert('No!');
-        alert(data.error);
-        console.log('Fail!');
-        if (typeof fail != 'undefined') {
-          fail(data);
-        }
-      }
-    })
-  }
-})
-.controller('MainCtrl', function($http, $scope) {
-  $scope.test = 'Scope Works!';
+      })
+    }
+  })
+  .controller('MainCtrl', function($http, $scope) {
+    $scope.test = 'Scope Works!';
 
-  // $scope.stories = [];
-  //
-  // $scope.appointments = info;
+    // $scope.stories = [];
+    //
+    // $scope.appointments = info;
 
-  // [{
-  //   name: 'Green Valley Clinic',
-  //   img_p: 'health.png',
-  //   date: 'June 30th',
-  //   time: 10,
-  //   zone: 'am'
-  // }, {
-  //   name: 'Silver Point Dental',
-  //   img_p: 'dental.png',
-  //   date: 'July 16th',
-  //   time: 2,
-  //   zone: 'pm'
-  // }, {
-  //   name: 'Multicare',
-  //   img_p: 'multicare.png',
-  //   date: 'July 25th',
-  //   time: 4,
-  //   zone: 'pm'
-  // }];
+    // [{
+    //   name: 'Green Valley Clinic',
+    //   img_p: 'health.png',
+    //   date: 'June 30th',
+    //   time: 10,
+    //   zone: 'am'
+    // }, {
+    //   name: 'Silver Point Dental',
+    //   img_p: 'dental.png',
+    //   date: 'July 16th',
+    //   time: 2,
+    //   zone: 'pm'
+    // }, {
+    //   name: 'Multicare',
+    //   img_p: 'multicare.png',
+    //   date: 'July 25th',
+    //   time: 4,
+    //   zone: 'pm'
+    // }];
 
-});
+  });
