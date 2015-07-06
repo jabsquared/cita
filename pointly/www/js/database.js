82da188d4bb9d7d293856fb2e23f5f2d351653b6
Louis Vichy <lgvichy@gmail.com>
2015-07-06 15:57:41 -0700

    Reload from Sign-in now work bitch

--- pointly/www/js/database.js
+++ pointly/www/js/database.js
@@ -1,38 +1,37 @@
 //console.log("called database script!");
 
-var doc;
 var info;
 
 //console.log("Ended database script!");
-setInterval(function(){
-
-  doc = $.ajax({
-    url       : 'https://api-us.clusterpoint.com//100600/Appointly/_list_last',
-    type      : 'GET',
-    dataType  : 'json',
-    // data      : '{"query": "<name>Test</name>"}',
-    beforeSend: function (xhr) {
-      xhr.setRequestHeader('Authorization', 'Basic ' + btoa('bpshonyak@live.com:Password01'));
-    },
-    success   : function (data) {
-      if (typeof success != 'undefined') {
-        // jQuery.parseJSON(doc.responseJSON.documents.toSource());
-        success(data);
+setInterval(function() {
+  if (userid != null) {
+    var doc = $.ajax({
+      url: 'https://api-us.clusterpoint.com//100600/Appointly/_list_last',
+      type: 'GET',
+      dataType: 'json',
+      // data      : '{"query": "<name>Test</name>"}',
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('Authorization', 'Basic ' + btoa('bpshonyak@live.com:Password01'));
+      },
+      success: function(data) {
+        if (typeof success != 'undefined') {
+          // jQuery.parseJSON(doc.responseJSON.documents.toSource());
+          success(data);
+        }
+      },
+      fail: function(data) {
+        alert(data.error);
+        console.log('Fail!');
+        if (typeof fail != 'undefined') {
+          fail(data);
+        }
       }
-    },
-    fail      : function (data) {
-      alert(data.error);
-      console.log('Fail!');
-      if (typeof fail != 'undefined') {
-        fail(data);
-      }
-    }
-  });
-
-  doc.done (function (data) {
-    info = $.grep( data.documents, function( n, i ) {
-      return n.user_id===userid;
     });
 
-  });
-},999);
+    doc.done(function(data) {
+      info = $.grep(data.documents, function(n, i) {
+        return n.user_id === userid;
+      });
+    });
+  }
+}, 999);
