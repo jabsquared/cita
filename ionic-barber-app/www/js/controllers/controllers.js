c11b5166ac6c43dbba49118fc004eef0cbe70c66
bpshonyak <bpshonyak@live.com>
2015-08-05 23:17:18 -0700

    move variables to schedule ctrl

--- ionic-barber-app/www/js/controllers/controllers.js
+++ ionic-barber-app/www/js/controllers/controllers.js
@@ -13,53 +13,19 @@ app.controller("AccountCtrl", function($scope, $rootScope, $state) {
     desc: "Profesional Barber"
   }];
 
-  $scope.drop = false;
-  $scope.drop2 = false;
-
   //Functions
-  $scope.toggle = function(num) {
-    if (num === 1) {
-      $scope.drop = !$scope.drop;
-    }
 
     $scope.schedule = function() {
       console.log('hit!');
       $state.go('schedule');
     }
-  }
-
-  // $scope.submitData = function() {
-  //   if ($scope.hasOwnProperty("barbers") !== true) {
-  //     $scope.barbers = [];
-  //   }
-  //   remoteBarberDB.post({
-  //       name: 'Gambino',
-  //       desc: 'This is where the barbers description would go. Their favorite styles to work with, history, and other information would all by written here. Maybe a short bio or something... idk. Lets add one more sentance to finish it off ;)'
-  //   });
-  // }
-
-  //Event Listeners
-  // $scope.$on('add', function(event, apt) {
-  //   $scope.barbers.push(apt);
-  // });
-  //
-  // $scope.$on('delete', function(event, id) {
-  //   for (var i = 0; i < $scope.barbers.length; i++) {
-  //     if ($scope.barbers[i]._id === id) {
-  //       $scope.barbers.splice(i, 1);
-  //     }
-  //   }
-  // });
 
 });
 
 app.controller("AppointmentsCtrl", function($scope, $state, $ionicPopup, $rootScope, aptListener) {
 
+  // Feilds
   $scope.appointments = [];
-
-  $scope.schedule_info = {};
-  $scope.schedule_info.alarm = true;
-  $scope.today = new Date();
 
   //Functions
 
@@ -80,7 +46,9 @@ app.controller("AppointmentsCtrl", function($scope, $state, $ionicPopup, $rootSc
 app.controller("ScheduleCtrl", function($scope, $state, $ionicPopup, aptListener, $ionicSideMenuDelegate) {
 
   //Feilds
-
+  $scope.schedule_info = {};
+  $scope.schedule_info.alarm = true;
+  $scope.today = new Date();
 
   //Functions
 
