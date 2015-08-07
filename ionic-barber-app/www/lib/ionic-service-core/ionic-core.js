00da518fbe13cc696ed8441c8c4c3fd33c31c26a
Louis Vichy <lgvichy@gmail.com>
2015-08-07 16:11:27 -0700

    Fixed ionic.io setting

--- ionic-barber-app/www/lib/ionic-service-core/ionic-core.js
+++ ionic-barber-app/www/lib/ionic-service-core/ionic-core.js
@@ -450,7 +450,7 @@ function($q, $ionicCoreSettings, $timeout, $http, persistentStorage, $ionicApp) 
 
 // Auto-generated configuration factory
 .factory('$ionicCoreSettings', function() {
-  var settings = {"app_id":"6bae2247","api_key":"ad495a92f74ab8e335181c3e71a587bd68c73d2ad803f2db"};
+  var settings = {"app_id":"dfe29437","api_key":"dd94b2f5fe8e13a916a59d0c08670f88c31c7923d664e1fd"};
   return {
     get: function(setting) {
       if (settings[setting]) {
