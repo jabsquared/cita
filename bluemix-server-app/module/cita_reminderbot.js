761b7c97cc3b7c806c74d3f24900dfbd044506f4
Louis Vichy <lgvichy@gmail.com>
2015-08-08 08:16:44 -0700

    Commented reminder bot

--- bluemix-server-app/module/cita_reminderbot.js
+++ bluemix-server-app/module/cita_reminderbot.js
@@ -4,27 +4,28 @@ var secret = require('./cita_secret');
 
 var PouchDB = require('pouchdb');
 
-var aptDB = (secret.cloudantAuth.user==="lab") ?
+var aptDB = (secret.cloudantAuth.user === "lab") ?
   new PouchDB("lab") : // Local testing
   new PouchDB(secret.cloudantAuth.url, {
     auth: {
       username: secret.cloudantAuth.user,
       password: secret.cloudantAuth.pass,
-  }
-});
+    }
+  });
 
 var InfiniteLoop = require('infinite-loop');
-var sender = require ('./cita_twilio');
+var sender = require('./cita_twilio');
 
 var il = new InfiniteLoop();
 
 var SMSBot = function() {
+  // Get the Current Date
   var nao = new Date();
-
+  // Extract the needed infomation from
   var naoymd =
     // nao.toISOString().substring(0, 11); // YMD
-  // nao.toISOString().substring(0, 13); // YMDH
-  nao.toISOString().substring(0, 17); // YMDHM
+    // nao.toISOString().substring(0, 13); // YMDH
+    nao.toISOString().substring(0, 17); // YMDHM
 
   // Filter by YMDH, client-side
   aptDB.allDocs({
@@ -35,17 +36,13 @@ var SMSBot = function() {
     if (err) {
       return console.log(err);
     }
-    // handle result
     // console.log("All The Responses:"); console.log(response);
-
+    // Go through all messages for the last D || H || M
     for (var i = 0; i < response.rows.length; i++) {
-
       // console.log("Responses on row " + i + " :");
       //   console.log(response.rows[i]);
       var theD = response.rows[i].doc;
-
       // console.log(theD);
-
       // If done return;
       if (!theD.alarm || theD.done) {
         return;
@@ -53,50 +50,42 @@ var SMSBot = function() {
       // The Appointment Date parsed into a Date Object
       var ad = new Date(theD.time);
       // console.log(ad);
-
       // If nao is > 6AM && 1st reminder == false
       if (nao.getHours() >= 6) {
-        if (theD.sms_0) {
+        if (theD.sms_0) { // Return if sms_0 has been sent
           return;
         }
         console.log("From The Beau Barbershop: You have an appoinment with " + theD.barber + " on " + ad.toTimeString('en-US', {
           hour: '2-digit',
           minute: '2-digit'
         }));
+        // Send Reminder SMS and toggle sms0
       }
-      // if sms0 is not done
-      // Send Reminder SMS && sms0 = true
-
-      // If nao is 30 minutes away && 2nd reminder == false
       // If nao is greater than (AppointmentTime - 30 MIN)
-
-      if ((ad.getTime() - nao.getTime()) < 3 * 10 * 60 * 1000) { // In MILISEC TODO
-        if (theD.sms_1) {
+      if ((ad.getTime() - nao.getTime()) < 3 * 10 * 60 * 1000) { //TODO
+        if (theD.sms_1) { // Return if sms_1 has been sent
           return;
         }
         console.log(theD.client_phone, "From The Beau Barbershop: You have an appoinment in 30 minutes with " +
           theD.barber + " on " +
           ad.toTimeString());
-
+          // Send Reminder SMS and toggle sms1
       }
-      // If sms1 is not done
-      // Send Reminder SMS && sms1 = true
-
       // If nao is greater than appTime, Done = true
-
       if (nao.getTime() > ad.getTime()) {
         var cp = theD.client_phone;
         console.log("DONE");
       }
-      // *****************************
     }
   });
 };
 
+// Set Timer:
 il.add(SMSBot, []).setInterval(9000).run();
 
 // 1800000 for 30 minutes
 
+// Change Checker:
 var changes = aptDB.changes({
   since: 'now',
   live: true,
@@ -105,7 +94,7 @@ var changes = aptDB.changes({
   // handle change
   var apt = change.doc;
 
-  if (apt.barber === null){
+  if (apt.barber === null) {
     return;
   }
 
@@ -118,7 +107,7 @@ var changes = aptDB.changes({
     //   hour: '2-digit',
     //   minute: '2-digit'
     // }) +
-    " with " +  apt.barber +
+    " with " + apt.barber +
     ". Have a nice day, " + apt.client_name + "!";
 
   // sendsms(0, "+12067909711", msg);
@@ -126,7 +115,7 @@ var changes = aptDB.changes({
   console.log(msg);
   // console.log(msg);
   // console.log(change);
-}).on('update',function (change) {
+}).on('update', function(change) {
   console.log(change);
 }).on('complete', function(info) {
   // changes() was canceled
