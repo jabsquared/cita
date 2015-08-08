351e079202d9770f8f6efe8dd51a7524ba612d4a
Louis Vichy <lgvichy@gmail.com>
2015-08-07 17:29:42 -0700

    Server-side stuffs

--- bluemix-server-app/app.js
+++ bluemix-server-app/app.js
@@ -56,6 +56,17 @@ var aptDB =
 //   console.log(err);
 // });
 
+var twilio = require('twilio');
+
+var twilioSid, twilioToken;
+
+vcapServices['user-provided'].forEach(function(service) {
+  // if (service.name == 'Twilio-9n') { // Release Twilio
+  if (service.name == 'Twilio-79') { // Test Twilio
+    twilioSid = service.credentials.accountSID;
+    twilioToken = service.credentials.authToken;
+  }
+});
 
 // Put a design doc into the db
 var InfiniteLoop = require('infinite-loop');
@@ -115,7 +126,7 @@ var ReminderBot = function() {
           client_name: theD.client_name,
           client_phone: theD.client_phone,
           barber: theD.barber,
-          time: theD.time,
+          time: ad,
           alarm: theD.alarm,
           sms_0: theD.sms_0,
           sms_1: true,
@@ -182,39 +193,31 @@ var changes = aptDB.changes({
   console.log(err);
 });
 
-var twilio = require('twilio');
-
-var twilioSid, twilioToken;
-
-vcapServices['user-provided'].forEach(function(service) {
-  // if (service.name == 'Twilio-9n') { // Release Twilio
-  if (service.name == 'Twilio-79') { // Test Twilio
-    twilioSid = service.credentials.accountSID;
-    twilioToken = service.credentials.authToken;
-  }
-});
-
 var sendsms = function(id, toNum, msg) {
   // var fromNum = '+13602343448';  // Bryan's #
   var fromNum = '+14696152255'; // LAB's free #
 
-  if (toNum == null)
+  if (!toNum)
   // toNum = "+12067909711"; // Bogdan's #
     toNum = "+12536422707"; // LAB's #
 
-  if (msg == null)
-    msg = "FROM BLUEMIX";
+  if (toNum[0]!=="+")
+    toNum = "+1" + toNum;
+
+  if (!msg)
+    msg = "FROM jabSquared";
 
   var client = new twilio.RestClient(twilioSid, twilioToken);
 
   client.sendMessage({
-    to: toNum,
+    to  : toNum,
     from: fromNum,
     body: msg
   }, function(err, message) {
     if (err) {
       console.error("Problem: " + message);
       console.log("Error: " + message);
+      sendsms(1,null,"Someone is messing with the Beau app");
       return;
     } else {
       console.log("Done");
