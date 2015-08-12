8fb214594e09f7b551a6f7b37652acb2bde88e7b
Louis Vichy <lgvichy@gmail.com>
2015-08-12 16:56:39 -0700

    added bool

--- bluemix-server-app/module/cita_twilio.js
+++ bluemix-server-app/module/cita_twilio.js
@@ -10,8 +10,10 @@ if (secret.twilioAuth.sID!=="lab"){
     secret.twilioAuth.token);
 }
 
+var lab = true;
+
 var sendSMS = function SendSMS(toNum, msg) {
-  if (secret.twilioAuth.sID==="lab" || true) {
+  if (secret.twilioAuth.sID==="lab" || lab) {
     console.log(msg);
     return;
   }
