c650f95e183d2e91d21c4c4bf4dfda9d1f0db5d1
bpshonyak <bpshonyak@live.com>
2015-09-12 04:06:39 -0700

    copied barber app to client app

--- ionic-client-app/www/js/app.js
+++ ionic-client-app/www/js/app.js
@@ -1,7 +1,9 @@
-var app = angular.module('starter', ['ionic', 'flexcalendar', 'pascalprecht.translate']);
+var app = angular.module('starter', ['ionic','ionic.service.core',  'flexcalendar', 'pascalprecht.translate']);
+// 'ionic.service.analytics',
 
 //instanciate databases
 var localAptDB = new PouchDB("appointments");
+
 var remoteAptDB = new PouchDB('https://itchentleverturearywhers:U7vFQNN2joOhU03Mw0iUx3SN @af48ada6-78db-4210-a80d-86619c82407e-bluemix.cloudant.com/appointments', {
   auth: {
     username: 'itchentleverturearywhers',
@@ -9,110 +11,94 @@ var remoteAptDB = new PouchDB('https://itchentleverturearywhers:U7vFQNN2joOhU03M
   }
 });
 
-var localBarberDB = new PouchDB("barbers");
-var remoteBarberDB = new PouchDB('https://itchentleverturearywhers:U7vFQNN2joOhU03Mw0iUx3SN @af48ada6-78db-4210-a80d-86619c82407e-bluemix.cloudant.com/barbers', {
-  auth: {
-    username: 'itchentleverturearywhers',
-    password: 'U7vFQNN2joOhU03Mw0iUx3SN'
-  }
-});
-
-//Local user database
-var localUserDB = new PouchDB("users");
-
-
+// $ionicAnalytics
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    // $ionicAnalytics.register();
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
-    //auto sync local and remote db's
-    localAptDB.sync(remoteAptDB, { live:true });
-    localBarberDB.sync(remoteBarberDB, { live:true });
+    localAptDB.sync(remoteAptDB, {
+      live: true,
+      retry: true
+    });
   });
-})
+});
 
 app.config(function($stateProvider, $urlRouterProvider, $translateProvider) {
 
-	$stateProvider
+  $stateProvider
 
-	.state('login', {
-		url: '/login',
-		templateUrl: 'templates/login.html',
-		controller: 'LoginCtrl'
-	})
+  .state('account', {
+    url: '/account',
+    templateUrl: 'templates/account.html',
+    controller: 'AccountCtrl'
+  })
 
-  .state('signup', {
-		url: '/signup',
-		templateUrl: 'templates/signup.html',
-		controller: 'SignupCtrl'
-	})
-
-	.state('account', {
-		url: '/account',
-		templateUrl: 'templates/account.html',
-		controller: 'AccountCtrl'
-	})
+  .state('appointments', {
+    url: '/appointments',
+    templateUrl: 'templates/appointments.html',
+    controller: 'AppointmentsCtrl'
+  })
 
   .state('schedule', {
-		url: '/schedule',
-		templateUrl: 'templates/schedule.html',
-		controller: 'ScheduleCtrl'
-	});
+    url: '/schedule',
+    templateUrl: 'templates/schedule.html',
+    controller: 'ScheduleCtrl'
+  });
 
-	$urlRouterProvider.otherwise('/login');
+  $urlRouterProvider.otherwise('/account');
 
   // Flex Calendar Language Options
 
   $translateProvider.translations('en', {
-   JANUARY: 'January',
-   FEBRUARY: 'February',
-   MARCH: 'March',
-   APRIL: 'April',
-   MAI: 'Mai',
-   JUNE: 'June',
-   JULY: 'July',
-   AUGUST: 'August',
-   SEPTEMBER: 'September',
-   OCTOBER: 'October',
-   NOVEMBER: 'November',
-   DECEMBER: 'December',
+    JANUARY: 'January',
+    FEBRUARY: 'February',
+    MARCH: 'March',
+    APRIL: 'April',
+    MAI: 'Mai',
+    JUNE: 'June',
+    JULY: 'July',
+    AUGUST: 'August',
+    SEPTEMBER: 'September',
+    OCTOBER: 'October',
+    NOVEMBER: 'November',
+    DECEMBER: 'December',
 
-   SUNDAY: 'Sunday',
-   MONDAY: 'Monday',
-   TUESDAY: 'Tuesday',
-   WEDNESDAY: 'Wednesday',
-   THURSDAY: 'Thurday',
-   FRIDAY: 'Friday',
-   SATURDAY: 'Saturday'
- });
- $translateProvider.translations('span', {
-     JANUARY: 'Enero',
-     FEBRUARY: 'Febrero',
-     MARCH: 'Marzo',
-     APRIL: 'Abril',
-     MAI: 'Mayo',
-     JUNE: 'Junio',
-     JULY: 'Julio',
-     AUGUST: 'Agosto',
-     SEPTEMBER: 'Septiembre',
-     OCTOBER: 'Octubre',
-     NOVEMBER: 'Noviembre',
-     DECEMBER: 'Diciembre',
+    SUNDAY: 'Sunday',
+    MONDAY: 'Monday',
+    TUESDAY: 'Tuesday',
+    WEDNESDAY: 'Wednesday',
+    THURSDAY: 'Thurday',
+    FRIDAY: 'Friday',
+    SATURDAY: 'Saturday'
+  });
+  $translateProvider.translations('span', {
+    JANUARY: 'Enero',
+    FEBRUARY: 'Febrero',
+    MARCH: 'Marzo',
+    APRIL: 'Abril',
+    MAI: 'Mayo',
+    JUNE: 'Junio',
+    JULY: 'Julio',
+    AUGUST: 'Agosto',
+    SEPTEMBER: 'Septiembre',
+    OCTOBER: 'Octubre',
+    NOVEMBER: 'Noviembre',
+    DECEMBER: 'Diciembre',
 
-     SUNDAY: 'domingo',
-     MONDAY: 'lunes',
-     TUESDAY: 'martes',
-     WEDNESDAY: 'miércoles',
-     THURSDAY: 'jueves',
-     FRIDAY: 'viernes',
-     SATURDAY: 'sábado'
- });
- $translateProvider.preferredLanguage('en');
- $translateProvider.useSanitizeValueStrategy('escape');
+    SUNDAY: 'domingo',
+    MONDAY: 'lunes',
+    TUESDAY: 'martes',
+    WEDNESDAY: 'miércoles',
+    THURSDAY: 'jueves',
+    FRIDAY: 'viernes',
+    SATURDAY: 'sábado'
+  });
+  $translateProvider.preferredLanguage('en');
+  $translateProvider.useSanitizeValueStrategy('escape');
 
 });
--- ionic-client-app/www/js/services/services.js
+++ ionic-client-app/www/js/services/services.js
@@ -1,89 +1,27 @@
-app.factory('aptListener', function($rootScope) {
 
-    localAptDB.changes({
-        continuous: true,
-        onChange: function(change) {
-            if (!change.deleted) {
-                $rootScope.$apply(function() {
-                    localAptDB.get(change.id, function(err, doc) {
-                        $rootScope.$apply(function() {
-                            if (err) console.log(err);
-                            $rootScope.$broadcast('add', doc);
-                        })
-                    });
-                })
-            } else {
-                $rootScope.$apply(function() {
-                    $rootScope.$broadcast('delete', change.id);
-                });
-            }
-        }
-    });
-    return true;
-});
-
-app.factory('barberListener', function($rootScope) {
-
-    localBarberDB.changes({
-        continuous: true,
-        onChange: function(change) {
-            if (!change.deleted) {
-                $rootScope.$apply(function() {
-                    localBarberDB.get(change.id, function(err, doc) {
-                        $rootScope.$apply(function() {
-                            if (err) console.log(err);
-                            $rootScope.$broadcast('add', doc);
-                        })
-                    });
-                })
-            } else {
-                $rootScope.$apply(function() {
-                    $rootScope.$broadcast('delete', change.id);
-                });
-            }
-        }
-    });
-    return true;
-});
-
-app.factory('userListener', function($rootScope) {
-
-    localUserDB.changes({
-        continuous: true,
-        onChange: function(change) {
-            if (!change.deleted) {
-                $rootScope.$apply(function() {
-                    localUserDB.get(change.id, function(err, doc) {
-                        $rootScope.$apply(function() {
-                            if (err) console.log(err);
-                            // $rootScope.$broadcast('add', doc);
-                               $rootScope.user = doc;
-                        })
-                    });
-                })
-            } else {
-                $rootScope.$apply(function() {
-                    $rootScope.$broadcast('delete', change.id);
-                });
-            }
-        }
-    });
-    return true;
-});
-
-app.service('userData', function () {
-        var user = {
-            name:    '',
-            email:   '',
-        }
-
-        return {
-            getUser: function () {
-                return user;
-            },
-            setUser: function(data) {
-                user.name    = data.name;
-                user.email   = data.email;
-            }
-        };
+app.factory('barberInfo', function() {
+  var barbers = [{
+    name: "Antonio",
+    desc: "Profesional Barber"
+  },{
+    name: "Gabino",
+    desc: "Profesional Barber"
+  }, {
+    name: "Matt",
+    desc: "Profesional Barber"
+  }, ];
+  var barber = "Gabino";
+  return {
+    setBaber: function(name) {
+      console.log('Setting Name');
+      barber = name;
+      console.log('Name: ' + barber);
+    },
+    getBarber: function() {
+      return barber;
+    },
+    getBarbers: function() {
+      return barbers;
+    }
+  };
 });
